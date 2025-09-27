5d8acaf4d88fc783e2b5be195551413ced96044a
Rahul Vishwakarma <rahulrjp16@gmail.com>
2025-09-27 22:32:14 +0530

    cors added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import router from "./routes/product.routes.js";
+import cors from "cors";
 
 const app = express()
-
+app.use(cors());
 app.set('view engine','ejs')
 app.use(express.static('public'))
 
