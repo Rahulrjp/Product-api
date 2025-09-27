7e0ec8ab5bc7ec64d6fd856ce563e393b60022da
Rahul Vishwakarma <rahulrjp16@gmail.com>
2025-09-27 23:11:00 +0530

    get one product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -35,3 +35,16 @@ export const getProducts = async (req, res) => {
     const data = await apiData
     res.status(200).json({ status: "success", length: data.length, data })
 }
+
+export const getOneProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await productModel.findById(id);
+        if (!product) {
+            return res.status(404).json({ status: "fail", message: "Product not found" });
+        }
+        res.status(200).json({ status: "success", data: product });
+    } catch (error) {
+        res.status(500).json({ status: "error", message: error.message });
+    }   
+}
--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProducts } from "../controllers/product.controller.js";
+import { getOneProduct, getProducts } from "../controllers/product.controller.js";
 import { getApiPage, getHomepage } from "../controllers/ui.controller.js";
 
 const router = Router();
@@ -7,5 +7,6 @@ const router = Router();
 router.route('/').get(getHomepage)
 router.route('/api').get(getApiPage)
 router.route('/api/products').get(getProducts)
+router.route('/api/products/:id').get(getOneProduct)
 
 export default router;
